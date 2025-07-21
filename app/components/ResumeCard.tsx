4c0a7ae8ba17433ab9ec4b8bac23dad5dc6afced
DiazSk <zaid07sk@gmail.com>
2025-07-21 12:50:03 -0700

    Enhance ResumeCard component styling for better responsiveness; adjust layout for smaller screens.

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -21,7 +21,7 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 max-sm:items-center max-sm:text-center">
                     {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
                     {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
                     {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
